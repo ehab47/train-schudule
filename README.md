9164a9c7ebca1e5ca4ade6a77f8725d1f3bc71f1
ehab moustafa <ehabmo36@gmail.com>
2018-06-16 09:53:04 -0700

    first page

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # train-schudule
+display train data & add more train
+use friebase to store train data
+use jquery to get data to page
+use moment to handel time
+bootstrap for styling
+
+// train name destinstinn frequency next arrival minutes away
+
+hard data
+train name
+destination
+freyuency
+first train time
+
+claclatio
+next arrival
+minutes away
+
+page load
+title paner add to page
+load data form firebase
+claclute each arrivel for each train
+minutes away
+display data
+
+- add train
+usre fail out four input 
+click submit
+collect data from jquery
+insert this data to fire base
+updata display data
---
+++ asstes/script.js
@@ -0,0 +1,81 @@
+ // Initialize 
+ var config = {
+    apiKey: "AIzaSyCjCbpfWKlvz2S3cl3lXYWDmzoS7c9EYRI",
+    authDomain: "ehab-e4301.firebaseapp.com",
+    databaseURL: "https://ehab-e4301.firebaseio.com",
+    projectId: "ehab-e4301",
+    storageBucket: "ehab-e4301.appspot.com",
+    messagingSenderId: "285355239098"
+  };
+  firebase.initializeApp(config);
+  var db = firebase.database()
+  db.ref("/train/1").set({
+      name : "test train1",
+      destination :"city",
+      frequency : 15,
+      fristtrain : "10,00,", 
+  })
+  // code blow add train to firebase
+  db.ref("/train/2").set({
+    name : "test train2",
+    destination :"city",
+    frequency : 30,
+    fristtrain : "10,00,", 
+})
+// how we get data out thier
+  db.ref('/train/').once('value').then(function(snapshot) {
+  console.log(snapshot.val())
+  var trains = snapshot.val()
+  for(train in trains){
+      console.log(trains[train])
+      $("#maintrain").append(trainToTr(trains[train]))
+  }
+
+ 
+  });
+
+  // take data for single train turne it to <tr>
+  // add that to table
+
+
+  function trainToTr(data){
+      var row =$("<tr>")
+      var name =$("<td>").text(data.name)
+      row.append(name)
+      
+      var destination =$("<td>").text(data.destination)
+      row.append(destination)
+
+      var frequency =$("<td>").text(data.frequency)
+      row.append(frequency)
+
+      var timeData=data.fristtrain.split(",")
+
+      var first=moment().hours(parseInt(timeData[0])).minutes(parseInt(timeData[1])).seconds(0)
+      var now=moment()
+
+      var departure = first;
+      var diff = departure.diff(now, 'minutes')
+      var freqInt = parseInt(data.frequency)
+
+        while(diff < 0) {
+            departure.add(freqInt, 'm')
+            diff = departure.diff(now, 'minutes')
+        }
+        
+      var next =$("<td>").text(departure.format("h: mm a"))
+      row.append(next)
+
+      var minutesAway = $('<td>').text(diff)
+      row.append(minutesAway)
+
+// this clclot nest arrivel inds td and appendd to row
+// colculat minutes awat and but in td useing momen js
+console.log(data.fristtrain.split(","))
+var testData=moment().hours(10).minutes(0).seconds(0).format("h: mm a")
+console.log(testData)
+      return row
+
+  }
+
+  
